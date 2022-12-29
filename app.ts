365f9c4ba00aa23941d31edd0aee12a5a157e4c4
Angel <angelsanchez.as524@gmail.com>
2022-12-28 16:53:20 -0800

    tuples in TS

--- app.ts
+++ app.ts
@@ -1,8 +1,20 @@
-const person = {
+const person : {
+    name: string;
+    age: number; 
+    hobbies: string[];
+    role: [number, string] //tuple type ; tells TS with exactly two elements the first should element should be a number and the second a string 
+} = {
     name: 'Angel',
     age: 29,
-    hobbies: ['Sports','Cooking']
+    hobbies: ['Sports','Cooking'], 
+    role: [2, 'author']
 };
+
+person.role.push('admin') // push an exception that is allowed in tupels 
+//person.role[1] = 10 this will not work 
+
+//use Tuples to be stricter and to know the amount of data is you are working with 
+
 let favoriteActivities: string[]; //declaring the type of string array type 
 favoriteActivities = ['Sports']; 
 console.log(person);
@@ -13,3 +25,5 @@ for(const hobby of person.hobbies) { //for of loop to accessing the hobbies prop
 
 //hobby will be identified as array of strings because if we go into an array of strings the individual values will have to be strings 
 //we will get an error if use the map method since we are not accesing arrays we are accessings strings 
+
+
