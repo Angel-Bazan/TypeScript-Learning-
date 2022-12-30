14e66306e20c51576660c775a46ddc5f1cba3ea1
Angel <angelsanchez.as524@gmail.com>
2022-12-30 15:04:41 -0800

    literal types

--- app.ts
+++ app.ts
@@ -1,10 +1,35 @@
-function add(n1: number | string, n2: number | string) {
+// function add(n1: number | string, n2: number | string) {
+//     let result;
+//     if(typeof n1 === 'number' && typeof n2 === 'number'){
+//         result = n1 + n2;
+//     }else{
+//         result = n1.toString() + n2.toString();
+//     }
+    
+//       return result;
+//   }
+function combine(
+    n1: number | string, 
+    n2: number | string,
+    resultConversion: 'as-number' | 'as-text' //any other values will not be allowed only of these values 
+    ) {
     let result;
-    if(typeof n1 === 'number' && typeof n2 === 'number'){
-        result = n1 + n2;
+    if(typeof n1 === 'number' && typeof n2 === 'number' || resultConversion === 'as-number' ){
+        result = +n1 + +n2; //forcing conversion to numbers by adding plus to each input 
     }else{
         result = n1.toString() + n2.toString();
     }
     
       return result;
   }
+
+  const combinedAges = combine(30, 26, 'as-number'); 
+  console.log(combinedAges); 
+
+  const combinedStringAges = combine('30', '26', 'as-number'); 
+  console.log(combinedStringAges); 
+
+  const combinedNames = combine('Max', 'Anna', 'as-text'); 
+  console.log(combinedNames);
+
+  //Literal types concept 
