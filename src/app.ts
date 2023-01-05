3844b637c55c11f097cf9196ed80b1c88c4b9131
Angel <angelsanchez.as524@gmail.com>
2023-01-05 14:58:19 -0800

    spread operator

--- src/app.ts
+++ src/app.ts
@@ -1,53 +1,22 @@
-// function add(n1: number | string, n2: number | string) {
-//     let result;
-//     if(typeof n1 === 'number' && typeof n2 === 'number'){
-//         result = n1 + n2;
-//     }else{
-//         result = n1.toString() + n2.toString();
-//     }
+function add(n1: number | string, n2: number | string) {
+    let result;
+    if(typeof n1 === 'number' && typeof n2 === 'number'){
+        result = n1 + n2;
+    }else{
+        result = n1.toString() + n2.toString();
+    }
     
-//       return result;
-//   }
+      return result;
+  }
 
 
 //   //Literal types concept 
 
 
-// function combine(
-//     n1: number | string, 
-//     n2: number | string,
-//     resultConversion: 'as-number' | 'as-text' //any other values will not be allowed only of these values 
-//     ) {
-//     let result;
-//     if(typeof n1 === 'number' && typeof n2 === 'number' || resultConversion === 'as-number' ){
-//         result = +n1 + +n2; //forcing conversion to numbers by adding plus to each input 
-//     }else{
-//         result = n1.toString() + n2.toString();
-//     }
-    
-//       return result;
-//   }
-
-//   const combinedAges = combine(30, 26, 'as-number'); 
-//   console.log(combinedAges); 
-
-//   const combinedStringAges = combine('30', '26', 'as-number'); 
-//   console.log(combinedStringAges); 
-
-//   const combinedNames = combine('Max', 'Anna', 'as-text'); 
-//   console.log(combinedNames);
-
-
-
-//type alias: before starting code we can use type alias to re-use them anywhere in our code; save time and can be cleaner 
-
-type Combinable = number | string; //any name can be used as long is not built in JS or TS
-type ConversionDescriptor = 'as-number' | 'as-text'; 
-
 function combine(
-    n1: Combinable, 
-    n2: Combinable,
-    resultConversion: ConversionDescriptor //any other values will not be allowed only of these values 
+    n1: number | string, 
+    n2: number | string,
+    resultConversion: 'as-number' | 'as-text' //any other values will not be allowed only of these values 
     ) {
     let result;
     if(typeof n1 === 'number' && typeof n2 === 'number' || resultConversion === 'as-number' ){
@@ -67,6 +36,37 @@ function combine(
 
   const combinedNames = combine('Max', 'Anna', 'as-text'); 
   console.log(combinedNames);
+
+
+
+//type alias: before starting code we can use type alias to re-use them anywhere in our code; save time and can be cleaner 
+
+// type Combinable = number | string; //any name can be used as long is not built in JS or TS
+// type ConversionDescriptor = 'as-number' | 'as-text'; 
+
+// function combine(
+//     n1: Combinable, 
+//     n2: Combinable,
+//     resultConversion: ConversionDescriptor //any other values will not be allowed only of these values 
+//     ) {
+//     let result;
+//     if(typeof n1 === 'number' && typeof n2 === 'number' || resultConversion === 'as-number' ){
+//         result = +n1 + +n2; //forcing conversion to numbers by adding plus to each input 
+//     }else{
+//         result = n1.toString() + n2.toString();
+//     }
+    
+//       return result;
+//   }
+
+//   const combinedAges = combine(30, 26, 'as-number'); 
+//   console.log(combinedAges); 
+
+//   const combinedStringAges = combine('30', '26', 'as-number'); 
+//   console.log(combinedStringAges); 
+
+//   const combinedNames = combine('Max', 'Anna', 'as-text'); 
+//   console.log(combinedNames);
 
   //Types Aliases & Object Types 
 //   Type aliases can be used to "create" your own types. You're not limited to storing union types though - you can also provide an alias to a (possibly complex) object type.
@@ -132,31 +132,43 @@ function combine(
 
 //function types call backs example:call back needs to be a function that maybe does'nt return anything but it does take a number as an argument 
 
-function add(n1: number, n2: number,) {
-    return n1 + n2;
-}
+// function add(n1: number, n2: number,) {
+//     return n1 + n2;
+// }
 
-function printResult(num: number): void {
-    console.log('Result: ' + num);
-}
+// function printResult(num: number): void {
+//     console.log('Result: ' + num);
+// }
 
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-    const result = n1 + n2;
-    cb(result);
-}
+// function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+//     const result = n1 + n2;
+//     cb(result);
+// }
 
-printResult(add(5, 12)); 
+// printResult(add(5, 12)); 
 
-let combineValues: (a: number, b:number) => number; 
+// let combineValues: (a: number, b:number) => number; 
 
-combineValues = add; 
+// combineValues = add; 
 
-console.log(combineValues(8, 8));
+// console.log(combineValues(8, 8));
 
-addAndHandle(10, 20, (result) => {
-    console.log(result);
-})
+// addAndHandle(10, 20, (result) => {
+//     console.log(result);
+// })
 
 
 //Unknown Types: for unknown types we first have to check the currently stored in userInput before we can assign it to any variable type; unknown is better than any because it allows you to do some checking of the different types being used  
 
+const button = document.querySelector('button')!;
+
+button.addEventListener('click', () => {
+  console.log('Clicked')
+})
+
+
+//spread operator 
+const hobbies = ['Sports', 'Cooking']; 
+const activeHobbies = ['Hiking']; 
+
+activeHobbies.push(...hobbies)
