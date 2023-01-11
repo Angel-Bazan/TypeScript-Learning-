83cd748488c6f9bd9b6a87149c6cab3f89129045
Angel <angelsanchez.as524@gmail.com>
2023-01-10 16:21:29 -0800

    creating a first class

--- src/app.ts
+++ src/app.ts
@@ -13,29 +13,29 @@
 //   //Literal types concept 
 
 
-function combine(
-    n1: number | string, 
-    n2: number | string,
-    resultConversion: 'as-number' | 'as-text' //any other values will not be allowed only of these values 
-    ) {
-    let result;
-    if(typeof n1 === 'number' && typeof n2 === 'number' || resultConversion === 'as-number' ){
-        result = +n1 + +n2; //forcing conversion to numbers by adding plus to each input 
-    }else{
-        result = n1.toString() + n2.toString();
-    }
+// function combine(
+//     n1: number | string, 
+//     n2: number | string,
+//     resultConversion: 'as-number' | 'as-text' //any other values will not be allowed only of these values 
+//     ) {
+//     let result;
+//     if(typeof n1 === 'number' && typeof n2 === 'number' || resultConversion === 'as-number' ){
+//         result = +n1 + +n2; //forcing conversion to numbers by adding plus to each input 
+//     }else{
+//         result = n1.toString() + n2.toString();
+//     }
     
-      return result;
-  }
+//       return result;
+//   }
 
-  const combinedAges = combine(30, 26, 'as-number'); 
-  console.log(combinedAges); 
+//   const combinedAges = combine(30, 26, 'as-number'); 
+//   console.log(combinedAges); 
 
-  const combinedStringAges = combine('30', '26', 'as-number'); 
-  console.log(combinedStringAges); 
+//   const combinedStringAges = combine('30', '26', 'as-number'); 
+//   console.log(combinedStringAges); 
 
-  const combinedNames = combine('Max', 'Anna', 'as-text'); 
-  console.log(combinedNames);
+//   const combinedNames = combine('Max', 'Anna', 'as-text'); 
+//   console.log(combinedNames);
 
 
 
@@ -178,11 +178,24 @@ activeHobbies.push(...hobbies)
 
 //Rest Operators example: With rest operators we can add an infinite amount of numbers with just one declaration
 
-const add = (...numbers: number[]) => {
-  return numbers.reduce((curResult, curValue) => {
-    return curResult + curValue;
-  }, 0);
-};
+// const add = (...numbers: number[]) => {
+//   return numbers.reduce((curResult, curValue) => {
+//     return curResult + curValue;
+//   }, 0);
+// };
 
-const addedNumbers = add(5, 10, 2, 3.7);
-console.log(addedNumbers)
+// const addedNumbers = add(5, 10, 2, 3.7);
+// console.log(addedNumbers)
+
+//Creating a first class 
+class Department {
+  name: string; 
+
+  constructor(n: string) {
+      this.name = n;
+  }
+}
+
+const accounting = new Department('Accounting');
+
+console.log(accounting);
